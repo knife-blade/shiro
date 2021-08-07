2007387835ab7681922e3eec282fa8fdb9433fd6
water <2716552863@qq.com>
2021-08-08 00:50:32 +0800

    [ADD]上传shiro裸项目

---
+++ shiro_without_cache/src/main/java/com/example/shiro/ShiroWithoutCacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroWithoutCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroWithoutCacheApplication.class, args);
+    }
+
+}
