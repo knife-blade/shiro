cef625a8870ad1999145c65fee625e4b7bd5c1d2
water <2716552863@qq.com>
2021-08-13 00:28:28 +0800

    [ADD]上传sql文件

--- shiro_1_annotation/src/main/java/com/example/shiro/ShiroApplication.java
+++ shiro_1_annotation/src/main/java/com/example/shiro/ShiroApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShiroWithoutCacheApplication {
+public class ShiroApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShiroWithoutCacheApplication.class, args);
+        SpringApplication.run(ShiroApplication.class, args);
     }
 
 }
