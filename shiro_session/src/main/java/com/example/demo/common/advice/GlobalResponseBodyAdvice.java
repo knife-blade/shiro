111226a3b23a0bd8c78a1ea07af81ac64cccb8a6
lyb <2716552863@qq.com>
2021-08-14 16:34:50 +0800

    [CHG]恢复错误处理

--- shiro_session/src/main/java/com/example/demo/common/advice/GlobalResponseBodyAdvice.java
+++ shiro_session/src/main/java/com/example/demo/common/advice/GlobalResponseBodyAdvice.java
@@ -1,8 +1,10 @@
 package com.example.demo.common.advice;
 
 import com.example.demo.common.entity.ResultWrapper;
+import com.example.demo.common.exception.BusinessException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -10,11 +12,14 @@ import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 import java.util.Arrays;
 import java.util.List;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalResponseBodyAdvice implements ResponseBodyAdvice<Object> {
     private List<String> KNIFE4J_URI = Arrays.asList(
@@ -22,8 +27,7 @@ public class GlobalResponseBodyAdvice implements ResponseBodyAdvice<Object> {
             "/swagger-resources",
             "/swagger-resources/configuration",
             "/v3/api-docs",
-            "/v2/api-docs",
-            "/webjars/**");
+            "/v2/api-docs");
     @Override
     public boolean supports(MethodParameter returnType,
                             Class<? extends HttpMessageConverter<?>> converterType) {
@@ -49,10 +53,24 @@ public class GlobalResponseBodyAdvice implements ResponseBodyAdvice<Object> {
         } else if (body instanceof ResultWrapper) {
             return body;
         } else if (isKnife4jUrl(request.getURI().getPath())) {
-            //如果是接口文档的uri，直接跳过
+            // 如果是接口文档uri，直接跳过
             return body;
         }
         return ResultWrapper.success().data(body);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResultWrapper<Object> handleException(Exception e){
+        log.error(e.getMessage(), e);
+
+        if (e instanceof BusinessException) {
+            return ResultWrapper.failure().message(e.getMessage());
+        } else {
+            // 实际项目中应该这样写，防止用户看到详细的异常信息
+            // return ResultWrapper.failure().message("操作失败");
+            return ResultWrapper.failure().message(e.getMessage());
+        }
     }
 
 
