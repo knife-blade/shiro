d24eeef2e6275eb3b87a4a863cde512fd7ab0182
water <2716552863@qq.com>
2021-08-16 19:54:37 +0800

    [CHG]修改接口的描述

--- shiro_2_origin_shiro-redis/src/main/java/com/example/demo/business/logout/LogoutController.java
+++ shiro_2_origin_shiro-redis/src/main/java/com/example/demo/business/logout/LogoutController.java
@@ -7,11 +7,11 @@ import org.apache.shiro.SecurityUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api(tags = "注销")
+@Api(tags = "登出")
 @RestController
 public class LogoutController {
 
-    @ApiOperation("注销")
+    @ApiOperation("登出")
     @PostMapping("logout")
     public ResultWrapper<Object> logout() {
         SecurityUtils.getSubject().logout();
