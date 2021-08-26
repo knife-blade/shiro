00e5b0f3cc74804254ca11b3a3883a7b9b7b9143
water <2716552863@qq.com>
2021-08-26 19:07:02 +0800

    [CHG]添加更多注释

--- shiro_3_jwt/src/main/java/com/example/demo/config/shiro/entity/AccountProfile.java
+++ shiro_3_jwt/src/main/java/com/example/demo/config/shiro/entity/AccountProfile.java
@@ -1,7 +1,13 @@
 package com.example.demo.config.shiro.entity;
 
+import com.example.demo.common.utils.ShiroUtil;
 import lombok.Data;
 
+/**
+ * 存放账户的信息。
+ *   在登录后会实例化一个此对象，然后放到subject里边。
+ *   获取方法：{@link ShiroUtil#getProfile()}
+ */
 @Data
 public class AccountProfile {
     private String id;
