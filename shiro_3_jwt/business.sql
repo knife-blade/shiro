e0c7cf04d78d0a8c4be9ed3519be2743a38b33a6
water <2716552863@qq.com>
2021-09-08 00:32:37 +0800

    [CHG]修改异常的引号

--- shiro_3_jwt/business.sql
+++ shiro_3_jwt/business.sql
@@ -45,21 +45,21 @@ CREATE TABLE t_role_permission_mid (
 ) charset=utf8 ENGINE=InnoDB;
 
 -- 密码：12345
-INSERT INTO `t_user` VALUES (1,''zhang3'',''a7d59dfc5332749cb801f86a24f5f590'',''e5ykFiNwShfCXvBRPr3wXg=='');
+INSERT INTO `t_user` VALUES (1,"zhang3","a7d59dfc5332749cb801f86a24f5f590","e5ykFiNwShfCXvBRPr3wXg==");
 -- 密码：abcde
-INSERT INTO `t_user` VALUES (2,''li4'',''43e28304197b9216e45ab1ce8dac831b'',''jPz19y7arvYIGhuUjsb6sQ=='');
-INSERT INTO `t_role` VALUES (1,''admin'',''超级管理员'');
-INSERT INTO `t_role` VALUES (2,''productManager'',''产品管理员'');
-INSERT INTO `t_role` VALUES (3,''orderManager'',''订单管理员'');
-INSERT INTO `t_permission` VALUES (1,''*:*'',''所有权限'');
-INSERT INTO `t_permission` VALUES (2,''product:add'',''增加产品'');
-INSERT INTO `t_permission` VALUES (3,''product:delete'',''删除产品'');
-INSERT INTO `t_permission` VALUES (4,''product:edit'',''编辑产品'');
-INSERT INTO `t_permission` VALUES (5,''product:view'',''查看产品'');
-INSERT INTO `t_permission` VALUES (6,''order:add'',''增加订单'');
-INSERT INTO `t_permission` VALUES (7,''order:delete'',''删除订单'');
-INSERT INTO `t_permission` VALUES (8,''order:edit'',''编辑订单'');
-INSERT INTO `t_permission` VALUES (9,''order:view'',''查看订单'');
+INSERT INTO `t_user` VALUES (2,"li4","43e28304197b9216e45ab1ce8dac831b","jPz19y7arvYIGhuUjsb6sQ==");
+INSERT INTO `t_role` VALUES (1,"admin","超级管理员");
+INSERT INTO `t_role` VALUES (2,"productManager","产品管理员");
+INSERT INTO `t_role` VALUES (3,"orderManager","订单管理员");
+INSERT INTO `t_permission` VALUES (1,"*:*","所有权限");
+INSERT INTO `t_permission` VALUES (2,"product:add","增加产品");
+INSERT INTO `t_permission` VALUES (3,"product:delete","删除产品");
+INSERT INTO `t_permission` VALUES (4,"product:edit","编辑产品");
+INSERT INTO `t_permission` VALUES (5,"product:view","查看产品");
+INSERT INTO `t_permission` VALUES (6,"order:add","增加订单");
+INSERT INTO `t_permission` VALUES (7,"order:delete","删除订单");
+INSERT INTO `t_permission` VALUES (8,"order:edit","编辑订单");
+INSERT INTO `t_permission` VALUES (9,"order:view","查看订单");
 INSERT INTO `t_user_role_mid` VALUES (1,2,2);
 INSERT INTO `t_user_role_mid` VALUES (2,1,1);
 INSERT INTO `t_role_permission_mid` VALUES (1,1,1);
