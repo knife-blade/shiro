187b24ff9cc62694b978ef22c81611fa44151852
lyb <2716552863@qq.com>
2021-09-28 15:25:23 +0800

    [CHG]不重要的修改

--- shiro_3_jwt/src/main/java/com/example/demo/common/util/auth/JwtUtil.java
+++ shiro_3_jwt/src/main/java/com/example/demo/common/util/auth/JwtUtil.java
@@ -2,12 +2,13 @@ package com.example.demo.common.util.auth;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.*;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
 import com.example.demo.common.util.ApplicationContextHolder;
 import com.example.demo.config.properties.JwtProperties;
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.Date;
 
